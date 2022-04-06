59047458a7504f2bca7f6daf32ecada2abbee928
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2022-04-06 01:18:04 +0000

    Updates for new GSL dev1 experiment
    - updated INFO file
    - updated UFS to 29Mar22 gsl/develop branch, c31f633
    - modified experiment setup scripts for PSLOT=rt_ufscam_l127_dev1

--- ush/rocoto/rt_l127_jet.sh
+++ ush/rocoto/rt_l127_jet.sh
@@ -1,12 +1,12 @@
 USER=rtfim
-GITDIR=/lfs4/BMC/gsd-fv3-dev/rtruns/UFS-CAMsuite_jet            ## where your git checkout is located
+GITDIR=/lfs4/BMC/gsd-fv3-dev/rtruns/UFS-CAMsuite_dev1            ## where your git checkout is located
 COMROT=$GITDIR/FV3GFSrun                                      ## default COMROT directory
 EXPDIR=$GITDIR/FV3GFSwfm                                      ## default EXPDIR directory
 ICSDIR=/lfs4/BMC/gsd-fv3-dev/rtruns/FV3ICS_L127/
 
-PSLOT=rt_ufscam_l127
-IDATE=2022032600
-EDATE=2022032600
+PSLOT=rt_ufscam_l127_dev1
+IDATE=2022040600
+EDATE=2022040600
 RESDET=768               ## 96 192 384 768
 
 ### gfs_cyc 1  00Z only;  gfs_cyc 2  00Z and 12Z
