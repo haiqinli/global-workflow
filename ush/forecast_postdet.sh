476828a3c1132e4d07305cba28066328fef170af
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2022-04-14 17:18:27 +0000

    Change location of CCN_ACTIVATE.BIN in ush/forecast_postdet.sh
      NOTE:  changed in 'if [ $imp_physics -eq 8 ]' section
        location is correct later in script when checking for GSL SDFs
        so will get linked later

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -289,7 +289,7 @@ EOF
   ####
   # copy CCN_ACTIVATE.BIN for Thompson microphysics
   if [ $imp_physics -eq 8 ]; then
-    $NCP $FV3INP/CCN_ACTIVATE.BIN  CCN_ACTIVATE.BIN
+    $NCP $FIX_AM/CCN_ACTIVATE.BIN  CCN_ACTIVATE.BIN
     ####
     $NCP $FIX_AM/freezeH2O.dat .
     $NCP $FIX_AM/qr_acr_qg.dat .
