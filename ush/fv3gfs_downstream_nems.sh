haiqinli/global-workflow

commit e480093446797e556bc1371f9f80610a7c9a6d4b

Add preamble, convert to bash, and remove env (#929)
This is the first in a wave of commits to improve and standardize the scripts within global workflow. In this commit, all scripts run during execution are converted to bash and a preamble is added to every script that is not sourced by another script.

Every script executed during a forecast cycle is converted to bash. This was mostly straightforward, though there were a couple Korne-shell conventions (primarily using `typeset` to format strings) that had to be replaced with bash-compatable alternatives like `printf`. This in turn required a few modification to prevent zero-padded numbers from being treated as octals (other may have been pre-existing bugs).

The preamble contains a number of feature to standardize code and improve script flow and debugging.

- First, it uses two variables, `$STRICT` and `$TRACE` to control the behavior of `set`. When `$STRICT` is `"YES"`, error on undefined variables (`set -u`) and exit on non-zero return (`set -e`) are turned on. When `$TRACE` is `"YES"`, command trace (`set -x`) is turned on and a useful string is set to `$PS4` that gives the name and line number of the script. Both `$STRICT` and `$TRACE` default to `"YES"`. They also set up commands, `$ERR_EXIT_ON` and `$TRACE_ON`, that will restore the setting of each in the event a script needs to temporarily turn them off.

- Second, the preamble sets up primative timing of scripts using Posix `date`.

- Third, it echos the script is beginning and at what time.

- Finally, it also establishes a postamble function and sets it to run as a trap of EXIT. The postamble will use the end time to calculate the run time of the script, then print that the script has ended at what time, how long has elapsed, and the exit code. By setting this up as a trap instead of just calling it at the end of the script, it ensures the postamble is called even if the script exits early because there is an error.

parm/config/config.base.nco.static
@@ -1,4 +1,4 @@
-#!/bin/ksh -x
+#! /usr/bin/env bash
 
 ########## config.base ##########
 # Common to all steps

ush/fv3gfs_downstream_nems.sh
@@ -1,5 +1,4 @@
-#!/bin/ksh
-set -x
+#! /usr/bin/env bash
 
 #-----------------------------------------------------------------------
 #-Hui-Ya Chuang, January 2014:  First version.
@@ -31,8 +30,7 @@ set -x
 #  1. Modify sea icea cover via land-sea mask.
 #-----------------------------------------------------------------------
 
-
-echo "!!!!!CREATING $RUN DOWNSTREAM PRODUCTS FOR FH = $FH !!!!!!"
+source "$HOMEgfs/ush/preamble.sh" "$FH"
 
 export downset=${downset:-1}
 export DATA=${DATA:-/ptmpd2/$LOGNAME/test}
@@ -130,16 +128,27 @@ while [ $nset -le $totalset ]; do
     # if final record of each piece is ugrd, add vgrd
     # copygb will only interpolate u and v together
     #$WGRIB2 -d $end $tmpfile |grep -i ugrd
-    $WGRIB2 -d $end $tmpfile |egrep -i "ugrd|ustm|uflx|u-gwd"
+    # grep returns 1 if no match is found, so temporarily turn off exit on non-zero rc
+    set +e
+    $WGRIB2 -d $end $tmpfile | egrep -i "ugrd|ustm|uflx|u-gwd"
     export rc=$?
+    ${ERR_EXIT_ON:-set -eu}
     if [[ $rc -eq 0 ]] ; then
       export end=$(expr ${end} + 1)
+    elif [[ $rc -gt 1 ]]; then
+      echo "FATAL: WGRIB2 failed with error code ${rc}"
+      exit $rc
     fi
-    # if final record is land, add next record icec
-    $WGRIB2 -d $end $tmpfile |egrep -i "land"
+    # if final record is land, add next record icec 
+    set +e
+    $WGRIB2 -d $end $tmpfile | egrep -i "land"
     export rc=$?
+    ${ERR_EXIT_ON:-set -eu}
     if [[ $rc -eq 0 ]] ; then
       export end=$(expr ${end} + 1)
+    elif [[ $rc -gt 1 ]]; then
+      echo "FATAL: WGRIB2 failed with error code ${rc}"
+      exit $rc
     fi
     if [ $iproc -eq $nproc ]; then
       export end=$ncount
@@ -176,6 +185,7 @@ while [ $nset -le $totalset ]; do
       echo "$nm $line" >> $DATA/poescript_srun
       nm=$((nm+1))
     done
+    nm=$(wc -l < $DATA/poescript_srun)
     ${launcher:-"srun --export=ALL"} -n $nm --multi-prog $DATA/poescript_srun
   else
     $launcher
@@ -282,9 +292,5 @@ while [ $nset -le $totalset ]; do
 
   export nset=$(expr $nset + 1 )
 done
-
-echo "!!!!!!CREATION OF SELECT $RUN DOWNSTREAM PRODUCTS COMPLETED FOR FHR = $FH !!!!!!!"
-#---------------------------------------------------------------
-
 
 exit 0

commit 4f02e2d8bee4e578246194cdfcff6165734dffc4

Updated INFO file
 Bring in additional commits to update to NOAA-EMC global-workflow develop branch, prototype/8 tag

   commit 17c8fa2e0518fddbb389b384efebb6a45bb71d79
   Author: Walter Kolczynski - NOAA <Walter.Kolczynski@noaa.gov>
   Date:   Tue Aug 9 10:36:50 2022 -0400

       Fix post comparison with zero-padded numbers (#964)

       An error was introduced with PR #929 that was causing 0p50 and 1p00 grib
       files to not be produced due to an error in comparing a zero-padded string.
       Switching to arithmatic comparison solves the issue. Also updated the method
       of the zero-padding to the preferred printf since the code was in close
       proximity.

   commit b90b2e9ae0f9421c7932c99d4d676e7890e3dfe9
   Author: Kate Friedman <kate.friedman@noaa.gov>
   Date:   Wed Aug 3 14:45:48 2022 -0400

       Remove SDATE=CDATE IAU block in NCO config.base (#963)

       - Remove the block in config.base.nco.static that checks if CDATE=SDATE and turns IAU settings to 0.
       - This block is not needed in operations and causes issues in pre-implementation developer testing when starting a new warm-started parallel with wave restarts.

       Refs: #960

INFO
@@ -1,3 +1,54 @@
+06 Dec 2022
+    - update to prototype/8 tag of global-workflow develop branch, 9a538ee
+18 Nov 2022
+    - update to 03Aug22 global-workflow develop branch, a658e75
+      * Updates for P8
+      * Fix GLDAS j-job link
+      * Add ocean post to archive dependencies
+      * Initial commit of directory comparison tools
+      * Convert scripts to bash and add preamble 
+      * Creat a data-atmosphere XML
+      * Remove obsolete platform references (Cray, Dell, WCOSS1, Theia)
+      * Add GDASApp capability to global-workflow  (Jedi-based atmospheric DA)
+      * Change gdasechgres dependency
+      * Optimize DA clone, build, and link
+      * Updates to WCOSS2 gempak ush scripts and cleanup of old release notes
+      * Add postsnd job when bufrsnd is on
+      * Update transfer list files from operations
+      * Fix history type of cldfra in diag tables
+      * Combine ecflow and rocoto workflow generation
+      * Remove new GSI scripts from gitignore
+      * Add ecFlow generator 
+      * Update gsi-utils hash to include bugfix in handling 3D sfc files
+      * Combine setup_workflows scripts
+      * Load `ncdiag` module instead of building it in the GSI, use UFSWM build system, pull sfcanl out from anal
+      * Update ice climo, fix option for non-fractional grid, and add cloud fraction for Thompson
+      * Update aerosol variable names and dust alpha
+      * Allow for wave mesh to be different than the ocean/ice mesh
+      * Migrate `jobs/`, `scripts/` and `ush/` from GSI to global-workflow
+      * Build GSI utilities from GSI-utils repo  
+      * Replace print_esmf with esmf_logkind
+      * Restore cycling capability
+      * Merge `ecf` changes from `WCOSS2`  (ecflow/ecf => ecf)
+      * Fix settings of dt_inner and lheatstrg
+      * Update buoy locations
+      * Update dust input files to not include _FillValue = NaN
+      * Replace --aerosols option with new apps, ATMA and S2SWA
+      * Update component versions 
+      * Update GSI and UPP build scripts
+      * Fix typo in parm/post file list
+      * Update caculation of restart based on if a wave IC exists
+      * Update from dev_v16 utils.f90. See issue #713
+      * Update GOCART settings for p8/p81
+      * Retire VSDB
+      * Add "atmos" COMPONENT subfolder to DMPDIR paths
+      * Move remaining global post scripts over from UPP (#771)
+      * Update readme for new linking procedure
+      * Consolidate post scripts to eliminate coupled mode of link script
+      * Updates for wave mesh cap
+      * Extend `rocoto.py` for handling offsets in data dependencies
+      * Refactor checkout script
+      * Update IC location for hera
 18 Sep 2022
     - change settings for suites FV3_GFS_v17_p8_gf_mynn and FV3_GFS_v17_p8_mynn
         sedi_semi=.false. (dt_inner will then become 75)

parm/config/config.base.nco.static
@@ -194,10 +194,6 @@ export IAU_OFFSET=6
 export DOIAU_ENKF=${DOIAU:-"YES"}   # Enable 4DIAU for EnKF ensemble
 export IAUFHRS_ENKF="3,6,9"
 export IAU_DELTHRS_ENKF=6
-if [[ "$SDATE" = "$CDATE" ]]; then
-  export IAU_OFFSET=0
-  export IAU_FHROT=0
-fi
 
 # Use Jacobians in eupd and thereby remove need to run eomg
 export lobsdiag_forenkf=".true."

ush/fv3gfs_downstream_nems.sh
@@ -77,10 +77,8 @@ elif [ $FH -eq 0 ] ; then
 else
   export paramlist=${paramlist:-$PARMpost/global_1x1_paramlist_g2}
   export paramlistb=${paramlistb:-$PARMpost/global_master-catchup_parmlist_g2}
-  export fhr3=$(expr $FH + 0 )
-  if [ $fhr3 -lt 100 ]; then export fhr3="0$fhr3"; fi
-  if [ $fhr3 -lt 10 ];  then export fhr3="0$fhr3"; fi
-  if [ $fhr3%${FHOUT_PGB} -eq 0 ]; then
+  export fhr3=$(printf "%03d" ${FH})
+  if (( fhr3%FHOUT_PGB == 0 )); then
     export PGBS=YES
   fi
 fi